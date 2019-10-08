f41ca139e01cf3c6b65259a79d8729bed02dad1c
laisut <laimonas.sutkus@gmail.com>
2019-10-08 11:35:57 +0300

    Bug fix.

--- aws_db_migration/mysql_exe.sh
+++ aws_db_migration/mysql_exe.sh
@@ -10,7 +10,7 @@ PORT=$5;
 DUMP_PATH=$6
 MYSQL_EXE=$7
 # Ensure that action type is supported by a provided mysql executable.
-MYSQL_ACTION_TYPE=$9
+MYSQL_ACTION_TYPE=$8
 
 if [[ "$MYSQL_ACTION_TYPE" = "BACKUP" ]]
 then
@@ -21,6 +21,6 @@ then
     echo "Using mysql to load a backup file."
     ${MYSQL_EXE} -u"$USERNAME" -p"$PASSWORD" -h"$HOST" -P"$PORT" "$DATABASE" < "$DUMP_PATH"
 else
-    echo "Unsupported event. Should be BACKUP or RESTORE."
+    echo "Unsupported event. Should be BACKUP or RESTORE. Given: $MYSQL_ACTION_TYPE."
     exit 127
 fi
